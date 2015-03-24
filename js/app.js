23c0ce9cc1906f0bfe904f14cc38d435d65018c9
alwesam <alwesam@gmail.com>
2015-03-23 21:05:03 -0700

    added player class, added enemy and player functions, instantiated enemies and player, added collision check

--- js/app.js
+++ js/app.js
@@ -1,8 +1,18 @@
 // Enemies our player must avoid
 var Enemy = function() {
-    // Variables applied to each of our instances go here,
-    // we've provided one for you to get started
-
+    //initial x-position (same)  
+    this.x = -200;
+    //initial y-position randomised across the three stone tiles
+    //70, 145, 225
+    //this.y = 225;
+    var yPos = function(){
+        var y = [65,145,225];
+        //var y = [100,175,250];
+        return y[Math.floor(Math.random()*y.length)];
+    }
+    this.y = yPos();
+    //generate a random speed between 40 inclusive and 200 exclusive
+    this.speed = 40+Math.floor(Math.random()*160);
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
     this.sprite = 'images/enemy-bug.png';
@@ -11,9 +21,8 @@ var Enemy = function() {
 // Update the enemy's position, required method for game
 // Parameter: dt, a time delta between ticks
 Enemy.prototype.update = function(dt) {
-    // You should multiply any movement by the dt parameter
-    // which will ensure the game runs at the same speed for
-    // all computers.
+    //console.log(dt);    
+    this.x += this.speed*dt;
 }
 
 // Draw the enemy on the screen, required method for game
@@ -21,16 +30,110 @@ Enemy.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 }
 
-// Now write your own player class
-// This class requires an update(), render() and
-// a handleInput() method.
+//get position of the enemy
+Enemy.prototype.getXPosition = function() {
+    return this.x;
+}
 
+Enemy.prototype.getYPosition = function() {
+    return this.y;
+}
 
-// Now instantiate your objects.
-// Place all enemy objects in an array called allEnemies
-// Place the player object in a variable called player
+/**
+*Player class
+**/
+var Player = function() {
+    //initial position
+    this.x = 200;
+    this.y = 400;
 
+    this.moveX=0;
+    this.moveY = 0;    
+    this.moveSpeed = 50;
 
+    this.sprite = 'images/char-boy.png';
+}
+
+//TODO clean up
+Player.prototype.update = function() {
+    //move palyer code
+    var changeX = player.getXPosition()+this.moveX*this.moveSpeed;
+    console.log(changeX);
+    if((changeX<500 && this.moveX==1) ||
+        (changeX>-50 && this.moveX==-1)) 
+        this.x += this.moveX*this.moveSpeed;
+
+    var changeY = player.getYPosition()+this.moveY*this.moveSpeed;
+    if((changeY<500 && this.moveY==1) ||
+        (changeY>0 && this.moveY==-1)) 
+        this.y += this.moveY*this.moveSpeed;
+
+    //stop after pressing, otherwise, it will move indefintely
+    this.moveX = 0; this.moveY = 0;
+}
+
+//get position of the player
+Player.prototype.getXPosition = function() {
+    return this.x;
+}
+
+Player.prototype.getYPosition = function() {
+    return this.y;
+}
+
+Player.prototype.setPosition = function (x,y) {
+    this.x = x;
+    this.y = y;
+}
+
+//check later
+Player.prototype.render = function() {
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+}
+
+Player.prototype.handleInput = function(hd) {
+    
+    if(hd=='right'){
+        this.moveX = 1;
+        this.moveY = 0;
+    } else if (hd == 'left') {
+        this.moveX = -1;
+        this.moveY = 0;
+    } else if (hd == 'up'){
+        this.moveX = 0;
+        this.moveY = -1;
+    } else if (hd == 'down') {
+        this.moveX = 0;
+        this.moveY = 1;
+    }
+}
+
+//add a new enemy every 3 seconds
+var allEnemies = [];
+setInterval(function(){allEnemies.push(new Enemy());},3000);
+
+//TODO improve collision detection
+var checkCollisions = function() {  
+
+        var playerPosX = player.getXPosition();
+        var playerPosY = player.getYPosition();
+        //console.log("Player: "+playerPosX+" "+playerPosY);
+        allEnemies.forEach(function(enemy) {
+            var enemyPosX = enemy.getXPosition();
+            var enemyPosY = enemy.getYPosition();
+            //console.log(enemyPosX+" "+enemyPosY);
+            if (Math.abs(enemyPosX-playerPosX)<20 &&
+                 Math.abs(enemyPosY-playerPosY)<40){
+                player.setPosition(200,400);
+                return;
+            }
+        });
+}
+
+//TODO make sure player doesn't exceed
+
+//instantiate a player
+var player = new Player();
 
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
